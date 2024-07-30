74054d2271ce829202abb11704498d5f9338bcf8
Sergey-py24 <chuvachoc2009@yandex.ru>
2024-07-30 21:24:51 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -3,7 +3,7 @@ _second=int(input('Введите второе число: '))
 _third=int(input('Введите третье число: '))
 if _first==_second and _second==_third:
     print('3')
-elif _first==_second or _second==_third:
+elif _first==_second or _second==_third or _first==_third:
     print('2')
 elif _first!=_second and _second!=_third:
     print('0')
