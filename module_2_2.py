f351d6f709f4f78f63895a0fa15486d48aa60efb
Sergey-py24 <chuvachoc2009@yandex.ru>
2024-07-30 20:57:09 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+_first=int(input('Введите первое число: '))
+_second=int(input('Введите второе число: '))
+_third=int(input('Введите третье число: '))
+if _first==_second and _second==_third:
+    print('3')
+elif _first==_second or _second==_third:
+    print('2')
+elif _first!=_second and _second!=_third:
+    print('0')
